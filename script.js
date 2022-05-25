88cdb0172b057ffc776190df5e37057756a10231
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2022-05-25 17:27:12 +0100

    Add auto slide feature

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const allSlids = document.querySelectorAll(".slider");
 const btnLeft = document.querySelector(".btn-left");
 const btnRight = document.querySelector(".btn-right");
 const btnRandom = document.querySelector(".btn-random");
+const AUTO_SLID_TIME = 2500;
+let intervalID;
 
 let curSlide = 1;
 const maxSlide = allSlids.length;
@@ -21,24 +23,44 @@ const addClass = function (arg) {
 };
 
 const nextSlid = function () {
+  stopInterval();
   remClas();
   if (curSlide === maxSlide) curSlide = 0;
   curSlide++;
   addClass(curSlide);
+  startInterval();
 };
 
 const prevSlid = function () {
+  stopInterval();
   remClas();
   if (curSlide === 1) curSlide = maxSlide + 1;
   curSlide--;
   addClass(curSlide);
+  startInterval();
 };
 
 const randomSlid = function () {
+  stopInterval();
   remClas();
   //   document.querySelector(`#slid--${random()}`).classList.add("slider--active");
   addClass(random());
+  startInterval();
 };
+
+const stopInterval = function () {
+  clearInterval(intervalID);
+};
+
+const startInterval = function () {
+  intervalID = setInterval(() => {
+    remClas();
+    if (curSlide === maxSlide) curSlide = 0;
+    curSlide++;
+    addClass(curSlide);
+  }, AUTO_SLID_TIME);
+};
+startInterval();
 
 btnRight.addEventListener("click", nextSlid);
 btnLeft.addEventListener("click", prevSlid);
